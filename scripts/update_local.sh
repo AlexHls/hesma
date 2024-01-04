0224bb4eaf123a1ab759fc8ca0b9a7a9c42a03af
Alexander Holas <alex.holas@gmx.de>
2024-01-04 17:40:50 +0100

    Add update scripts

---
+++ scripts/update_local.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Shutting down the local cluster..."
+docker compose -f local.yml down
+
+echo "Updating the local cluster..."
+git fetch origin
+git pull origin main
+
+echo "Applying migrations..."
+docker compose -f local.yml run django python manage.py migrate
+
+echo "Starting the local cluster..."
+docker compose -f local.yml up -d --build
+
+echo "Update complete!"
