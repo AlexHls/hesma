93d8ad5d7cf13be8ae10b70fe4944474eab90b0a
Alexander Holas <alex.holas@gmx.de>
2024-01-05 10:26:17 +0100

    Add news as data

--- hesma/pages/tests/test_models.py
+++ hesma/pages/tests/test_models.py
@@ -1,6 +1,9 @@
-from django.test import TestCase
+import datetime
 
-from hesma.pages.models import FAQ, FAQTopic
+from django.test import TestCase
+from django.utils import timezone
+
+from hesma.pages.models import FAQ, FAQTopic, News
 
 
 class FAQTopicModelTestCase(TestCase):
@@ -36,3 +39,26 @@ class FAQModelTestCase(TestCase):
         self.topic.delete()
         with self.assertRaises(FAQ.DoesNotExist):
             FAQ.objects.get(pk=self.faq.pk)
+
+
+class NewsModelTestCase(TestCase):
+    def setUp(self):
+        self.news = News.objects.create(
+            title="Test News",
+            content="This is a test news",
+            date=timezone.now(),
+        )
+
+    def test_news_str(self):
+        self.assertEqual(str(self.news), "Test News")
+
+    def test_news_was_published_recently(self):
+        self.assertTrue(self.news.was_published_recently())
+
+    def test_news_was_published_recently_future(self):
+        self.news.date = timezone.now() + datetime.timedelta(days=1)
+        self.assertFalse(self.news.was_published_recently())
+
+    def test_news_was_published_recently_old(self):
+        self.news.date = timezone.now() - datetime.timedelta(days=15)
+        self.assertFalse(self.news.was_published_recently())
