e10c4ddcbdb53f6369f4eef52beda5e319b46d0f
Alexander Holas <70367168+AlexHls@users.noreply.github.com>
2024-01-10 21:26:11 +0100

    Remove manifest staticfiles

--- config/settings/production.py
+++ config/settings/production.py
@@ -55,7 +55,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 # MEDIA
