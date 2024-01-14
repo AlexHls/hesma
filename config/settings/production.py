29d7e26d07feb595f7afb84b3218b7aed82e1084
Alexander Holas <alex.holas@gmx.de>
2024-01-14 17:48:29 +0100

    Revert default fromemail

--- config/settings/production.py
+++ config/settings/production.py
@@ -66,7 +66,7 @@ STORAGES = {
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-from-email
 DEFAULT_FROM_EMAIL = env(
     "DJANGO_DEFAULT_FROM_EMAIL",
-    default="Hesma <hesma@h-its.org>",
+    default="Hesma <noreply@hesma.h-its.org>",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#server-email
 SERVER_EMAIL = env("DJANGO_SERVER_EMAIL", default=DEFAULT_FROM_EMAIL)
