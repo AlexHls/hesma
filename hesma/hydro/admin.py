b2477b1c26032e914a5fdd895f806aa773f642ff
Alexander Holas <alex.holas@gmx.de>
2024-04-06 16:54:50 +0200

    Add admin action for hydro1d

--- hesma/hydro/admin.py
+++ hesma/hydro/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from hesma.hydro.models import HydroSimulation, HydroSimulation1DModelFile
 
+
+@admin.action(description="Generate interactive plot")
+def generate_interactive_plot(modeladmin, request, queryset):
+    for obj in queryset:
+        if obj.is_valid_hesma_file:
+            obj.interactive_plot = obj.get_plot_json()
+        obj.save()
+
+
+@admin.register(HydroSimulation1DModelFile)
+class HydroSimulation1DModelFileAdmin(admin.ModelAdmin):
+    actions = [generate_interactive_plot]
+
+
 admin.site.register(HydroSimulation)
-admin.site.register(HydroSimulation1DModelFile)
